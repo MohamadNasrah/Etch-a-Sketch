6d2d131025d4e1619b630ae5679a6aacde0b0f80
MohamadNasrah <mohamad.nasrah@gmail.com>
2024-05-14 01:32:01 +0300

    Add mouse button hold featue
    add a feature that would only draw on canvas when mouse button is held.

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
 const container = document.querySelector(".container");
+const body = document.querySelector("body");
+let state;
+
+
+let mouseHold = body.addEventListener("mousedown", (e)=> { //checks if mouse is held down
+    e.preventDefault()
+    state = true;
+    console.log(state)
+})
+let mouseRelease = body.addEventListener("mouseup", (e)=> { //checks if mouse is released
+    e.preventDefault()
+    state = false;
+    console.log(state)
+})
 let pixel;
 function draw() {
     for (let i = 0; i < 256; i++) {
-        pixel = document.createElement("dix");
+        pixel = document.createElement("div");
         pixel.setAttribute("class", "pixel");
         container.appendChild(pixel);
     }
     container.addEventListener("mouseover", (e)=> {
-        
+       if (state) { //this if statement is to make sure that the mouse only leaves a trail if the mouse is held down
         if (e.target == container) {
             return;
         } else{
-            e.target.style.backgroundColor = "black"
+            e.target.classList.add("hovered")
         }
- 
+        }
     })
 }
 
 
 
-
-
-
-
-
-/*
-function draw() {
-    for (let i = 0; i < 16; i++ ) {
-        const row = document.createElement("div");
-        //row.setAttribute("class", "pixel");
-        row.setAttribute("class", "pixel row");
-        container.appendChild(row);
-        for (let j = 0; j < 15; j++) {
-            const colmn = document.createElement("div");
-            //colmn.setAttribute("class", "pixel");
-            colmn.setAttribute("class", "pixel colmn")
-            row.appendChild(colmn);
-        }
-    }
-}
-*/
+draw();
